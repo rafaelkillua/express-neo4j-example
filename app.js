4506e7cb8baaae035c25778723b8ce188f5c4fdc
Rafael Sampaio Tavares <rafael_killua@msn.com>
2020-08-03 22:15:13 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+require('dotenv').config()
+
+const indexRouter = require('./src/routes/index')
+
+const app = express()
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+app.use('/', indexRouter)
+
+module.exports = app
